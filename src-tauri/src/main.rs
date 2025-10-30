08e4eb2776ec1fbb38a54a1a5d2c58a6d5dc247e
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2025-10-30 15:58:34 +0800

    delete
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- src-tauri/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
-fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_barcode_scanner::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
