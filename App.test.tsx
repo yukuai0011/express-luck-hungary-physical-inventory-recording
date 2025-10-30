ed32cb76acae4ac29dd76302d8499401560e69bb
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2025-10-30 11:32:16 +0800

    chore: remove Expo React Native app scaffold and related files
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
-
-test('renders App without crashing', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toBeTruthy();
-});
